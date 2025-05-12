c4b8550370bb25848eb70d91284c0c8ad464b534
Rajkumar Neupane <84633852+raiz-toff@users.noreply.github.com>
2025-05-12 19:45:25 -0400

    remvoed Ps1 file renaming

--- TOOLS/Bulkrenaming.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -File | ForEach-Object {
-    $newName = $_.Name -replace ' ', ''
-    Rename-Item $_.FullName -NewName $newName
-}
-# This script renames all files in the current directory by removing spaces from their names.
-# It uses Get-ChildItem to list all files and ForEach-Object to iterate over each file.
